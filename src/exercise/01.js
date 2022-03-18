b66ccf8749b345417347014311a29a4690f8de4b
Maicon Silva <maiconrs95@gmail.com>
2022-03-18 11:20:09 -0300

    feat: exercise 01.js

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,11 +3,11 @@
 
 import * as React from 'react'
 
-function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+function Greeting({defaultValue = ''}) {
+  const [name, setName] = React.useState(defaultValue)
 
   function handleChange(event) {
+    setName(event?.target?.value)
     // 🐨 update the name here based on event.target.value
   }
 
@@ -15,7 +15,7 @@ function Greeting() {
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name} />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -23,7 +23,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting defaultValue="Maicon " />
 }
 
 export default App
